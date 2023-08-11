1fe4f0de7bd26e2b82f99cf65e61170947dc15f9
Mariano Portal <67043011+devsiderio@users.noreply.github.com>
2023-08-11 13:41:18 -0300

    mongo (#1)
    make first steps with mongo

---
+++ models/User.js
@@ -0,0 +1,19 @@
+import { model, Schema } from "mongoose";
+
+let collection = "users";
+
+let schema = new Schema({
+  name: { type: String, required: true },
+  lastName: { type: String },
+  mail: { type: String, required: true, unique: true },
+  photo: {
+    type: String,
+    default:
+      "https://www.cinemascomics.com/wp-content/uploads/2020/06/poder-darth-vader.jpg",
+  },
+  password: { type: String, required: true },
+  country: { type: String, required: true },
+});
+
+let User = model(collection, schema);
+export default User;

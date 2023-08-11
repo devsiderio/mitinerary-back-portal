72ff0541b27b2339162d08b86b69143daaccc220
Mariano <devsiderio@tutanota.com>
2023-08-11 09:48:58 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mytinerary-back-portal
+# mytinerary-back
+
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import userRouter from "./users.js";
+
+let router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Index" });
+});
+router.use("/users", userRouter);
+
+export default router;
